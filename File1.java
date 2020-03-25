5fe213401a8d84b2301f305c90a399bc25999224
Samyak Jain <samjain4@publicisgroupe.net>
2020-03-26 02:36:36 +0530

    Initial File Commit

---
+++ File1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class File1
+
+{
+public static void main()
+{
+int a,b;
+Scanner y =new Scanner(System.in);
+System.out.println("We create a demo calculator");
+System.out.println("Input the numbers");
+a=y.nextInt();
+b=y.nextInt();
+}
+}
